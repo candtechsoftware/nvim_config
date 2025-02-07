5ac71d5303ffebb9a53f1c49c00b2d7b7db7899a
Alexmatthew Candelario <alex@candtechsoftware.com>
2025-02-07 10:03:00 -0500

    new color stuff

--- lua/candtech/init.lua
+++ lua/candtech/init.lua
@@ -33,3 +33,32 @@ autocmd({"BufWritePre"}, {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+vim.opt.guicursor = "a:block"
+
+
+local lspconfig = require('lspconfig')
+
+lspconfig.zls.setup{
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  cmd = {"zls"},
+  filetypes = {"zig"},
+  root_dir = lspconfig.util.root_pattern("build.zig", ".git"),
+  settings = {
+    zls = {
+      semantic_tokens = true,
+      enable_definition = true
+    }
+  }
+}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<leader>vi", function() vim.lsp.buf.incoming_calls() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
