1ca11876bf16eae4c3fb5c7cbbeafba0dd7c1c86
Alexmatthew Candelario <alex@candtechsoftware.com>
2025-02-17 01:56:07 -0500

    lazy is back

--- lua/candtech/lazy/colors.lua
+++ lua/candtech/lazy/colors.lua
@@ -1,8 +1,24 @@
+function ColorMyPencils()
+    vim.cmd('colorscheme github_dark')
+    vim.api.nvim_set_hl(0, "Normal", { bg = "black" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "black" })
+
+end
+
+
 return {
-    'sainnhe/sonokai',
-    config = function()
-        vim.cmd('colorscheme sonokai')
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
+    {
+        'projekt0n/github-nvim-theme',
+        name = 'github-theme',
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require('github-theme').setup({
+                -- ...
+            })
+
+            ColorMyPencils()
+        end,
+    }
+
 }
