4671d20870c54e974d1919dd5e80c042fbb70b33
Alexmatthew Candelario <alex@candtechsoftware.com>
2026-04-09 19:38:30 -0400

    first commit

--- ftdetect/jai.lua
+++ ftdetect/jai.lua
@@ -1,6 +1,31 @@
 -- Jai filetype detection + options (native, no plugin)
 vim.filetype.add({ extension = { jai = "jai" } })
 
+-- Simple brace-aware indentexpr for Jai.
+-- cindent misreads `x: Type` as a C goto-label, so we roll our own.
+function _G._jai_indentexpr()
+  local lnum = vim.v.lnum
+  local prev = vim.fn.prevnonblank(lnum - 1)
+  if prev == 0 then return 0 end
+
+  local prev_line = vim.fn.getline(prev)
+  local indent = vim.fn.indent(prev)
+  local sw = vim.bo.shiftwidth
+
+  -- Increase indent after lines ending with { or (
+  if prev_line:match('[{(]%s*$') or prev_line:match('[{(]%s*//.*$') then
+    indent = indent + sw
+  end
+
+  -- Decrease indent for lines starting with } or )
+  local cur_line = vim.fn.getline(lnum)
+  if cur_line:match('^%s*[})]') then
+    indent = indent - sw
+  end
+
+  return math.max(0, indent)
+end
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "jai",
   callback = function()
@@ -8,9 +33,9 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.bo.shiftwidth = 4
     vim.bo.tabstop = 4
     vim.bo.expandtab = true
-    vim.bo.cindent = true
     vim.bo.smartindent = false
-    vim.bo.cinoptions = 't0,:0,l1,(0,Ws'
+    vim.bo.cindent = false
+    vim.bo.indentexpr = 'v:lua._jai_indentexpr()'
     vim.bo.errorformat = "%f:%l\\,%c:%m"
     vim.bo.define = "^\\s*\\w\\+\\s*:.*:.*\\s*[({]"
   end,
